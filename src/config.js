82f58cd9b0526e63ce7074f2d83dc06935885f2d
JinwooSeong <seongjinwoo76@yahoo.com>
2021-09-08 18:43:06 +0800

    added english documentation

--- src/config.js
+++ src/config.js
@@ -2,4 +2,3 @@ export const alTokenAddress = "0xF92Cb2FA7ff7E20b1f4D92aa70A2101887F57075"
 export const tokenAddresses = {"MATIC":"0x7cFB55c3eC29Ee8AeD4c5DB5819dbB8D4bd243B0","WETH":"0xE0347174d68b21a9e6D1b439FA0bBea2Fd233760","BTCB":"0xaDb88FCc910aBfb2c03B49EE2087e7D6C2Ddb2E9"}
 export const bandPriceOracleAddress = "0x455323351Ead16bB1928B18c54674887C7f4b67F"
 export const lendingPoolAddress = "0x074A985A466dBf9494a606bef2c0BA7e33F53129"
-    
